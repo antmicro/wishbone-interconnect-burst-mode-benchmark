48a17f12e3a8072b91535e998003b22528c847ef
Rafal Kolucki <rkolucki@antmicro.com>
2022-03-04 17:25:10 +0100

    Fix inc. addr. burst tests without tail operation

--- test/tests/test-incrementing.py
+++ test/tests/test-incrementing.py
@@ -28,11 +28,14 @@ async def test_read(dut):
     adr_offset = int(os.environ.get("adr_offset", "4")) # offset from which you want to start (byte addressed)
     length = int(os.environ.get("length", "8"))
     bte = int(os.environ.get("bte", "0"))
-    # initial RAM contents, starting from offset with wrap lsb masked out
+    sram_fill = int(os.environ.get("sram_fill", "0"))
+    fifo_fill = int(os.environ.get("fifo_fill", "0"))
+
+    # prepare data and addresses
     prefill = random.sample(range(0x1, 0xffffffff), length)
 
     # bus data writes
-    bus_read = [None for i in prefill]
+    bus_read = [None for i in range(length)]
 
     # setup
     harness = WbMaster(dut)
@@ -43,15 +46,9 @@ async def test_read(dut):
     await harness.sram_write(adr_offset, prefill, wrap_bitmask(bte))
     responses = await harness.wb_inc_adr_burst_cycle(adr_base + adr_offset, bus_read, acktimeout=3, bte=bte)
 
-    # get operations addresses in execution order
-    adr_verify = []
-    for res in responses:
-        adr_verify.append(res.adr)
-
     # verify if bus reads match initial RAM contents
-    adr_verify_start = min(adr_verify)
-    for i in range(len(adr_verify)):
-        assert responses[i].datrd == prefill[adr_verify[i]-adr_verify_start]
+    for i in range(len(prefill)):
+        assert responses[i].datrd == prefill[i]
 
     clk_gen.kill()
 
@@ -75,14 +72,9 @@ async def test_write(dut):
     responses = await harness.wb_inc_adr_burst_cycle(adr_base + adr_offset, bus_write, acktimeout=3, bte=bte)
     sram_read = await harness.sram_read(adr_offset, len(bus_write), wrap_bitmask(bte))
 
-    # get operations addresses in execution order
-    adr_verify = []
-    for res in responses:
-        adr_verify.append(res.adr)
-
     # verify if RAM contents match bus writes
-    for i in range(len(adr_verify)):
-        assert bus_write[i] == sram_read[i]
+    for i in range(len(bus_write)):
+        assert responses[i].datwr == sram_read[i]
 
     clk_gen.kill()
 
@@ -112,15 +104,12 @@ async def test_read_with_write_tail(dut):
     responses = await harness.wb_inc_adr_burst_cycle(adr_base + adr_offset, bus_read, acktimeout=3, bte=bte, end=tail)
     sram_post = await harness.sram_read(tail[0] - adr_base, 1)
 
-    # get operations addresses in execution order
-    adr_verify = []
-    for res in responses[:-1]:
-        adr_verify.append(res.adr)
+    # get burst operations results sorted by address
+    responses_sorted = sorted(responses[:-1], key=lambda a: (a.adr))
 
     # verify if bus reads match initial RAM contents
-    adr_verify_start = min(adr_verify)
-    for i in range(len(adr_verify)):
-        assert responses[i].datrd == prefill[adr_verify[i]-adr_verify_start]
+    for i in range(len(prefill)):
+        assert responses_sorted[i].datrd == prefill[i]
 
     # verify end operation (changed RAM word match tail write)
     assert sram_post[0] == tail[1]
@@ -149,13 +138,8 @@ async def test_write_with_read_tail(dut):
     responses = await harness.wb_inc_adr_burst_cycle(adr_base + adr_offset, bus_write, acktimeout=3, bte=bte, end=tail)
     sram_read = await harness.sram_read(adr_offset, len(bus_write), wrap_bitmask(bte))
 
-    # get operations addresses in execution order
-    adr_verify = []
-    for res in responses[:-1]:
-        adr_verify.append(res.adr)
-
     # verify if RAM contents match bus writes
-    for i in range(len(adr_verify)):
+    for i in range(len(bus_write)):
         assert bus_write[i] == sram_read[i]
 
     # verify end operation (read RAM word match previous write)
